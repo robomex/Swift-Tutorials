a91cfa92617150f742bf27a56827b92bc236dd17
robomex <atmorales@gmail.com>
2014-07-07 00:38:30 -0500

    added double,double return to dictionary

--- RayWenderlichTut3.playground/section-1.swift
+++ RayWenderlichTut3.playground/section-1.swift
@@ -26,3 +26,39 @@ func calcTipWithTipPct(tipPct:Double) -> (tipAmt: Double, total:Double) {
 calcTipWithTipPct(0.20)
 */
 
+class TipCalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total:Double, taxPct:Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct:Double) -> (tipAmt: Double, total: Double) {
+        let tipAmt = subtotal * tipPct
+        let finalTotal = total + tipAmt
+        return(tipAmt, finalTotal)
+    }
+    
+    func returnPossibleTips() -> Dictionary<Int, (Double, Double)> {
+        
+        let possibleTips = [0.15, 0.18, 0.20]
+        
+        var retval = Dictionary<Int, (Double, Double)>()
+        for possibleTip in possibleTips {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
+
+let tipCalc = TipCalculatorModel(total: 21.19, taxPct: 0.06)
+tipCalc.returnPossibleTips()
