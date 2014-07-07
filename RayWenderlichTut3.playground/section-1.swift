d7b3f5306fcc491fab0ec40aa92076ef84c8383d
robomex <atmorales@gmail.com>
2014-07-06 23:46:31 -0500

    Ray Wenderlich Swift Tutorial 3 playground

---
+++ RayWenderlichTut3.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Ray Wenderlich Tutorial 3 - Playground
+
+/*
+let tipAndTotal = (4.00, 25.19)
+
+tipAndTotal.0
+tipAndTotal.1
+
+let (theTipAmt, theTotal) = tipAndTotal
+theTipAmt
+theTotal
+
+let tipAndTotalNamed = (tipAmt: 4.00, total: 25.19)
+tipAndTotalNamed.tipAmt
+tipAndTotalNamed.total
+
+
+let total = 21.19
+let taxPct = 0.06
+let subtotal = total / (taxPct + 1)
+func calcTipWithTipPct(tipPct:Double) -> (tipAmt: Double, total:Double) {
+    let tipAmt = subtotal * tipPct
+    let finalTotal = total + tipAmt
+    return (tipAmt, finalTotal)
+}
+calcTipWithTipPct(0.20)
+*/
+
