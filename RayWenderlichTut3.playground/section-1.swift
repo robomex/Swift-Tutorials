beb64c21ae591d30dc22b547e04342a013bd8a1b
robomex <atmorales@gmail.com>
2014-07-07 01:19:18 -0500

    table view controllers tutorial

--- RayWenderlichTut3.playground/section-1.swift
+++ RayWenderlichTut3.playground/section-1.swift
@@ -26,6 +26,8 @@ func calcTipWithTipPct(tipPct:Double) -> (tipAmt: Double, total:Double) {
 calcTipWithTipPct(0.20)
 */
 
+import Foundation
+
 class TipCalculatorModel {
     
     var total: Double
@@ -62,3 +64,51 @@ class TipCalculatorModel {
 
 let tipCalc = TipCalculatorModel(total: 21.19, taxPct: 0.06)
 tipCalc.returnPossibleTips()
+
+// 1
+import UIKit
+
+// 2
+class TestDataSource: NSObject, UITableViewDataSource {
+    
+    // 3
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    var possibleTips = Dictionary<Int, (tipAmt:Double, total:Double)>()
+    var sortedKeys:Int[] = []
+    
+    // 4
+    init() {
+        possibleTips = tipCalc.returnPossibleTips()
+        sortedKeys = sort(Array(possibleTips.keys))
+        super.init()
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return sortedKeys.count
+    }
+        
+    // 1
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        // 2
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: nil)
+            
+        // 3
+        let tipPct = sorted[indexPath.row]
+            
+        // 4
+        let tipAmt = possibleTips[tipPct]!.tipAmt
+        let total = possibleTips[tipPct]!.total
+            
+        // 5
+        cell.textLabel.text = "\(tipPct)%:"
+        cell.detailTextLabel.text = String(format:"Tip: $%0.2f, Total: $%0.2f", tipAmt, total)
+        return cell
+    }
+    
+}
+
+
+let testDataSource = TestDataSource()
+let tableView = UITableView(frame:CGRect(x: 0, y: 0, width: 320, height: 320), style: .Plain)
+tableView.dataSource = testDataSource
+tableView.reloadData()
